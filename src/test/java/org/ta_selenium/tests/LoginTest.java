a84a904b6e0d259559ca9dae2724ddc56b59fa88
g.martino <gianmart83@gmail.com>
2024-07-26 16:28:46 +0200

    add testSuccessfullLogin

--- src/test/java/org/ta_selenium/tests/LoginTest.java
+++ src/test/java/org/ta_selenium/tests/LoginTest.java
@@ -1,4 +1,19 @@
 package org.ta_selenium.tests;
 
-public class LoginTest {
+import org.ta_selenium.pages.LoginPage;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void testSuccessfulLogin() {
+        String expectedUrl = "https://demoqa.com/profile";
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.goToLoginBookStore();
+        loginPage.compileForm("TestAutomation", "*Ta_Java0");
+        loginPage.clickLoginBtn();
+        String actualUrl = loginPage.getActualUrl();
+        Assert.assertEquals(actualUrl, expectedUrl, "The profile page URL is incorrect");
+    }
 }
