a6cc9e3d658ad021e110f378fe247a96464419cd
g.martino <gianmart83@gmail.com>
2024-07-26 16:23:47 +0200

    add logic to manage driver

--- src/main/java/org/ta_selenium/utils/DriverFactory.java
+++ src/main/java/org/ta_selenium/utils/DriverFactory.java
@@ -1,4 +1,19 @@
 package org.ta_selenium.utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class DriverFactory {
+    public static WebDriver getChrDriver() {
+        WebDriverManager.chromedriver().create();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-cache");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--incognito");
+        return new ChromeDriver(options);
+    }
 }
--- src/test/java/org/ta_selenium/tests/BaseTest.java
+++ src/test/java/org/ta_selenium/tests/BaseTest.java
@@ -1,4 +1,22 @@
 package org.ta_selenium.tests;
 
+import org.ta_selenium.utils.DriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = DriverFactory.getChrDriver();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
